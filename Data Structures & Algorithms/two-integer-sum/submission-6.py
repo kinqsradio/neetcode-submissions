791ca1d7aa02a55060c3b72580280d845a3058cc
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2026-04-06 17:21:11 +1000

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:        
+        hash_map = {}
+        for i in range(len(nums)):
+            complimentary = target - nums[i]
+            if complimentary in hash_map:
+                return [hash_map[complimentary], i]
+            hash_map[nums[i]] = i
+        return []
