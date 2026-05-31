01a523b30b9b5d70c3ef6256aee7be983efc4267
Anh Dang <anhdang.contact@gmail.com>
2026-06-01 08:54:44 +1000

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 2 pointers
+        # l, r = 0, len(nums) -1
+
+        # while l <= r:
+        #     m = l + ((r-l) //2 )
+        #     if nums[m] > target:
+        #         r = m-1
+        #     elif nums[m] < target:
+        #         l = m+1
+        #     else:
+        #         return m
+
+        # return -1
+
+        # bruteforce
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
