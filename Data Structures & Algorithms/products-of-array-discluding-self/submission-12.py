75759cf5f54168819c65005edaf583fbc8b68e47
Anh Dang <anhdang.contact@gmail.com>
2026-06-01 17:05:56 +1000

    Update: products-of-array-discluding-self - submission-12

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-12.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-12.py
@@ -3,12 +3,23 @@ class Solution:
         n = len(nums)
         res = [0] * n
 
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j: continue
-                prod *= nums[j]
+        # for i in range(n):
+        #     prod = 1
+        #     for j in range(n):
+        #         if i == j: continue
+        #         prod *= nums[j]
 
-            res[i] = prod
+        #     res[i] = prod
+
+        # return res
+        
+        prefix=1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix =1
+        for i in range(n-1, -1 ,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
 
         return res
