18f8adef7e628404be107422e24763c59daa5afd
Anh Dang <anhdang.contact@gmail.com>
2026-06-02 02:09:04 +1000

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,28 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:        
+        # BruteForce
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+        
+        # Hashmap
+        # hash_map = {}
+        # for i in range(len(nums)):
+        #     diff = target - nums[i]
+        #     if diff in hash_map:
+        #         return [hash_map[diff], i]
+        #     hash_map[nums[i]] = i
+        # return []
+
+        # hashmap using enumerate
+        hmap = {}
+        for i, v in enumerate(nums):
+            diff = target - v
+            if diff in hmap:
+                return [hmap[diff], i]
+            hmap[v] = i
+
+        return []
+        
