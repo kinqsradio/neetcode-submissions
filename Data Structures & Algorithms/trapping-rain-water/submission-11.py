f0f0c801b2552072a0e1864ae6c35cc85e419a3e
Anh Dang <anhdang.contact@gmail.com>
2026-06-02 11:54:21 +1000

    Add: trapping-rain-water - submission-11

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-11.py
@@ -0,0 +1,46 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # bruteforce
+        # if not height: return 0
+        # result = 0
+        # for i in range(len(height)):
+        #     leftMax = rightMax = height[i]
+
+        #     for j in range(i):
+        #         leftMax = max(leftMax, height[j])
+        #     for j in range(i+1, len(height)):
+        #         rightMax = max(rightMax, height[j])
+        #     result += min(leftMax, rightMax) - height[i]
+        # return result
+
+        # 2 pointers
+        # if not height: return 0
+
+        # l, r = 0, len(height)-1
+        # res = 0
+        # maxLeft, maxRight = height[l] , height[r]
+        # while l<r: 
+        #     if maxLeft < maxRight:
+        #         l+=1
+        #         maxLeft = max(maxLeft, height[l])
+        #         res += maxLeft - height[l]
+        #     else:
+        #         r-=1
+        #         maxRight = max(maxRight, height[r])
+        #         res+= maxRight - height[r]
+        # return res
+
+        if not height: return 0
+        l, r = 0, len(height)-1
+        res = 0 
+        lMax, rMax = height[l], height[r]
+        while l<r:
+            if lMax < rMax:
+                l+=1
+                lMax = max(lMax, height[l])
+                res += lMax - height[l]
+            else:
+                r-=1
+                rMax = max(rMax, height[r])
+                res += rMax - height[r]
+        return res
